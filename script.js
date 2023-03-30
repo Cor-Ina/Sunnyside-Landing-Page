abcfa4a113c55fcd33191598a44f74be65b1b111
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-03-30 19:43:19 -0400

    (Almost) final version

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
-    document.querySelector(".front-page").classList.toggle("change");
-});
+    document.querySelector(".hamburger-container").classList.toggle("change");
+    const sidebar = document.querySelector(".sidebar");
+    if (sidebar.style.display === "block") {
+      sidebar.style.display = "none";
+    } else {
+      sidebar.style.display = "block";
+    }
+  });
