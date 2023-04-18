3a65a9d4dd334e08149d47337ef25d33a3e123f4
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-04-17 21:29:51 -0400

    Final

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 
-document.querySelector(".hamburger-menu").addEventListener("click", () => {
-    document.querySelector(".hamburger-container").classList.toggle("change");
-    const sidebar = document.querySelector(".sidebar");
+const sidebar = document.querySelector(".sidebar");
+
+document.querySelector(".hamburger-container").addEventListener("click", () => {
+    // document.querySelector(".hamburger-container").classList.toggle("change");
     if (sidebar.style.display === "block") {
       sidebar.style.display = "none";
     } else {
       sidebar.style.display = "block";
     }
+});
+
+document.querySelector(".sidebar-x i").addEventListener("click", () => {
+  sidebar.style.display = "none";
 });
